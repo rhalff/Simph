2e009d35cfd58f82aae1bfb3dd5213a074461dbd
Rob Halff <rob.halff@gmail.com>
2011-05-30 17:29:26 +0200

    Add some more example pages

---
+++ widgets/hello_world.html.php
@@ -0,0 +1,10 @@
+<h1>Hello World</h1>
+<p>
+Welcome to the Simph HelloWorld widget
+</p>
+<p>
+I'm included by using my {HELLO_WORLD} tag.
+</p>
+<p>
+I can contain PHP logic, but in this case I'm using none.
+</p>
